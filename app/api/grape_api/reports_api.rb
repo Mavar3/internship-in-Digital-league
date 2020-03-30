16f474ee45df0fee377622eea80b920268b1c69b
Матвей Симаков (Mavar) <simakov_matvey@mail.ru>
2020-03-30 17:35:27 +0300

    Добавлены косметические изменения

--- app/api/grape_api/reports_api.rb
+++ app/api/grape_api/reports_api.rb
@@ -5,7 +5,6 @@ class GrapeApi
     namespace :reports do
       desc 'Получение отчета о виртуальных пользователя',
         failure: [{ code: 404, message: 'Пользователь с заказами не найден' }]
-        # failure: [{ code: 406, message: '`Счётчик не может быть < 1' }]
       params do
         optional :count, type: Integer, desc: 'Количество vm в отчёте'
         optional :usr, type: Integer, desc: 'Формирование по определённому юзеру.'
@@ -13,18 +12,13 @@ class GrapeApi
       get do
         user = User.find_by_id(params[:usr])
         error!({ message: 'Пользователь с заказами не найден' }, 404) unless user
-        # error!({ message: 'Счётчик не может быть < 1' }, 406) unless params[:count] > 0
-        # orders = Order.find_by(user_id: params[:usr].to_i)
-
         ReportWorker.perform_async(params[:usr].to_i, params[:count].to_i)
         present user.report
-        # present Order.where(user_id: params[:usr]).order(:cost)
       end
 
       route_param :result, type: String do
         get do
           present Order.all.order(:cost)
-          # present Order.all #, with: GrapeApi::Entities::Report
         end
       end
 
--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -2,7 +2,12 @@ class ReportWorker
   include Sidekiq::Worker
 
   def perform(user_id, counter)
-    # really_slow_task(orders, user_id, counter)
+    create_report(user_id, counter)
+  end
+
+  private
+ 
+  def create_report(user_id, counter)
     orders = Order.all
     report = Report.new(user_id: user_id)
     sort_orders = orders.order(:cost)
@@ -11,20 +16,4 @@ class ReportWorker
     report.min_cost = sort_orders.last(counter)
     report.save
   end
-
-  # private
-
-  # def really_slow_task(orders, user_id, counter)
-    # report = Report.new(user_id: user_id)
-    # sort_orders = orders.order(:cost)
-    # report.max_cost = sort_orders.first(counter)
-    # report.min_cost = sort_orders.last(counter)
-    # report.save
-  # end
-
-  # def form_ord(orders)
-    # all_cost = {}
-    # orders.each { |order| all_cost[order.id] = order.cost}
-    # return all_cost
-  # end
 end
