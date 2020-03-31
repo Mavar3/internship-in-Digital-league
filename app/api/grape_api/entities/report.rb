3f8596c2c66ae03d9be1d47077c48430d511a94e
Матвей Симаков (Mavar) <simakov_matvey@mail.ru>
2020-03-31 19:42:50 +0300

    FIX

--- app/api/grape_api/entities/report.rb
+++ app/api/grape_api/entities/report.rb
@@ -1,12 +1,11 @@
 class GrapeApi
   module Entities
     class Report < Grape::Entity
-      link = 0
-      expose :get_link, if: lambda { |object, options|  (link = options[:link]) != nil } 
+      # expose :get_link, if: lambda { |object, options|  options[:link] != nil } 
 
-      def get_link
-        "http://localhost:3000/api/reports/#{link}"
-      end
+      # def get_link
+      #   "http://localhost:3000/api/reports/#{link}"
+      # end
     end
   end
 end
